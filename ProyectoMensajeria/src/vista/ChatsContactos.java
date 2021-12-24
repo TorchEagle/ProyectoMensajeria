9abf3b4cb5417b1772d3edfaa4c3721da3343637
paugi <paugi@DESKTOP-VOA6UF1>
2021-12-24 17:55:14 +0100

    Añadido listas

--- ProyectoMensajeria/src/vista/ChatsContactos.java
+++ ProyectoMensajeria/src/vista/ChatsContactos.java
@@ -3,6 +3,10 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.border.TitledBorder;
+import javax.swing.AbstractListModel;
+import java.awt.Color;
 
 public class ChatsContactos extends JDialog {
 
@@ -27,7 +31,33 @@ public class ChatsContactos extends JDialog {
 	 * Create the dialog.
 	 */
 	public ChatsContactos() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 558, 387);
+		getContentPane().setLayout(null);
+		
+		JList list = new JList();
+		list.setForeground(Color.BLACK);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"1, 2, 3, 4"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list.setBorder(new TitledBorder(null, "Privados", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		list.setBounds(10, 199, 152, -187);
+		getContentPane().add(list);
+		
+		JList list_1 = new JList();
+		list_1.setBorder(new TitledBorder(null, "Grupales", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		list_1.setBounds(194, 199, 152, -187);
+		getContentPane().add(list_1);
+		
+		JList list_2 = new JList();
+		list_2.setBorder(new TitledBorder(null, "Contactos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		list_2.setBounds(377, 199, 152, -187);
+		getContentPane().add(list_2);
 
 	}
 
